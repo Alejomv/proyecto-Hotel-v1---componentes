227ba29072cfdee2c2cc5a23916e9a538ad7c4fd
Alejomv <amartinezvelez@gmail.com>
2023-09-01 15:31:24 -0500

    Moviles_Hotel_1_Parte

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './header/header'
+import Pie from './pie/pie'
+import Cuerpo from './cuerpo/cuerpo'
+import Menu from './menu/menu'
+
+function App() {
+   return <>
+     <Menu/>
+     <Header/>
+     <Cuerpo/>
+     <Pie/>
+   </>
+}
+
+export default App
